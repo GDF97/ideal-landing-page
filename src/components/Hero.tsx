3c44aeaf4f518f20ea36db3234096735aff738c0
gdf97 <pedrohsilva1233@gmail.com>
2026-09-15 11:02:04 -0300

    :lipstick: feat: Adicionando responsividade no Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,24 +4,24 @@ import Logo2 from "../assets/ideal-logo-2.png";
 function Hero() {
   return (
     <section
-      className="p-4 w-full flex justify-center items-center gap-8 min-h-screen"
+      className="p-4 pt-8 w-full flex justify-center items-center gap-8 lg:min-h-screen"
       id="home"
     >
       <article className="flex flex-col gap-8 justify-start">
-        <h1 className="text-8xl font-anton">
+        <h1 className="font-anton text-6xl lg:text-8xl">
           TUDO PARA
           <p>CONSTRUIR</p>
           <p className="text-ideal-yellow">SEUS SONHOS.</p>
         </h1>
-        <p className="w-120 text-xl">
+        <p className="lg:text-xl lg:w-120">
           Materiais de construção com qualidade e preço justo para sua obra do
           início ao acabamento.
         </p>
-        <button className="w-fit py-4 px-6 bg-ideal-yellow text-ideal-black text-lg font-inter uppercase cursor-pointer font-semibold">
+        <button className="w-full lg:w-fit py-4 px-6 bg-ideal-yellow text-ideal-black text-lg font-inter uppercase cursor-pointer font-semibold">
           Ver produtos {"->"}
         </button>
       </article>
-      <article className="relative">
+      <article className="hidden lg:block lg:relative">
         <div className="absolute w-32 h-32 bg-ideal-white inset-0"></div>
         <div className="absolute w-32 h-32 bg-ideal-white right-0 bottom-0"></div>
         <div className="absolute bg-ideal-yellow right-0 top-0 py-4 px-6 w-55">
