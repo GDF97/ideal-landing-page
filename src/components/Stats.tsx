d7640bc81cb115d6b75b0c3ede3fd62e3cd3da26
gdf97 <pedrohsilva1233@gmail.com>
2026-09-15 11:03:13 -0300

    :lipstick: feat: Adicionando responsividade na Stats section

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -12,11 +12,11 @@ function Card({
   desc: String;
 }) {
   return (
-    <div className="flex items-center gap-4">
-      <img src={image} alt="img" className="w-25 h-25" />
+    <div className="w-full lg:w-fit flex items-start lg:items-center gap-4">
+      <img src={image} alt="img" className="w-15 h-15 lg:w-25 lg:h-25" />
       <div className="flex flex-col gap-2.5">
-        <h1 className="text-[20px] font-semibold">{title}</h1>
-        <p className="w-50">{desc}</p>
+        <h1 className="lg:text-[20px] font-semibold">{title}</h1>
+        <p className="lg:w-50">{desc}</p>
       </div>
     </div>
   );
@@ -24,19 +24,20 @@ function Card({
 
 function Stats() {
   return (
-    <section className="w-full h-45 bg-ideal-black text-white flex items-center justify-center gap-12">
+    <section className="w-full h-fit px-4 py-8  bg-ideal-black text-white flex flex-col gap-6 items-start justify-center lg:gap-12 lg:h-45 lg:flex-row">
       <Card
         image={Shield}
         title={"Qualidade"}
         desc={"Produtos das melhores marcas do mercado."}
       />
-      <div className="w-px h-25 bg-ideal-white opacity-25"></div>
+      <div className="w-full h-px lg:w-px lg:h-25 bg-ideal-white opacity-25"></div>
       <Card
         image={Hands}
         title={"Atendimento Especializado"}
         desc={"Profissionais prontos para te ajudar sempre."}
       />
-      <div className="w-px h-25 bg-ideal-white opacity-25"></div>
+      <div className="w-full h-px lg:w-px lg:h-25 bg-ideal-white opacity-25"></div>
+
       <Card
         image={Star}
         title={"Condições Especiais"}
